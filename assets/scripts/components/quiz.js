04db10d458894124041d12f81becee8e12d048af
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-01-22 15:16:20 +0300

    правки квиза

--- assets/scripts/components/quiz.js
+++ assets/scripts/components/quiz.js
@@ -65,18 +65,22 @@ function quizInit () {
   let currentSlide = 0
   let classChosen
   const nextBtns = quiz.querySelectorAll('[data-element="quiz-next"]')
+  const prevBtns = quiz.querySelectorAll('[data-element="quiz-prev"]')
   const slides = quiz.querySelectorAll('[data-element="quiz-inner"]')
   const form = quiz.querySelector('[data-element="quiz-form"]')
+  const wrap = quiz.querySelector('.quiz__wrap')
   const progress = quiz.querySelector('[data-element="quiz-progress"]')
   const progressFill = quiz.querySelector('[data-element="quiz-progress-bar-fill"]')
 
   function initValidate () {
     const button = slides[currentSlide].querySelector("[data-element='quiz-next']")
     const input = slides[currentSlide].getElementsByTagName('input')
-    if (input.length === 1) {
-      input[0].addEventListener('input', function () {
-        button.disabled = !this.value
-      })
+    if (input[0].type === 'text') {
+      for (let i = 0; i < input.length; i++) {
+        input[i].addEventListener('input', function () {
+          button.disabled = !input[0].value || !input[1].value
+        })
+      }
     } else {
       for (let i = 0; i < input.length; i++) {
         input[i].addEventListener('input', function () {
@@ -87,28 +91,67 @@ function quizInit () {
     }
   }
 
+  function initValidateLastSlide () {
+    const inputs = slides[currentSlide].querySelectorAll('input')
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('change', validate)
+    }
+    function validate () {
+      if (inputs[inputs.length - 1].checked) {
+        for (let i = 0; i < inputs.length - 1; i++) {
+          inputs[i].disabled = true
+          inputs[i].closest('.quiz__label').classList.add('disable')
+        }
+      } else {
+        for (let i = 0; i < inputs.length - 1; i++) {
+          inputs[i].disabled = false
+          inputs[i].closest('.quiz__label').classList.remove('disable')
+        }
+      }
+      let disableLastInput = false
+      for (let i = 0; i < inputs.length - 1; i++) {
+        if (inputs[i].checked) {
+          inputs[inputs.length - 1].disabled = true
+          inputs[inputs.length - 1].closest('.quiz__label').classList.add('disable')
+          disableLastInput = true
+        }
+      }
+      if (!disableLastInput) {
+        inputs[inputs.length - 1].disabled = false
+        inputs[inputs.length - 1].closest('.quiz__label').classList.remove('disable')
+      }
+    }
+  }
+
   for (let i = 0; i < nextBtns.length; i++) {
     nextBtns[i].addEventListener('click', nextSlide)
+  }
+
+  for (let i = 0; i < prevBtns.length; i++) {
+    prevBtns[i].addEventListener('click', prevSlide)
   }
 
   initValidate()
 
   function nextSlide () {
-    if (currentSlide === 0) progress.classList.remove('hide')
+    if (currentSlide === 0) {
+      progress.classList.remove('hide')
+      wrap.classList.remove('quiz__wrap_first-slide')
+    }
     if (currentSlide === 1) {
       classChosen = slides[currentSlide].querySelector('input:checked').value
+      if (classChosen === "0") sendData()
     }
     if (currentSlide === 2) {
-      if (classChosen === "0") {
-        currentSlide += 2
+      if (slides[currentSlide].querySelector('input:checked').value === "Да") {
+        currentSlide += 1
+        const title = slides[currentSlide + 1].querySelector('.quiz__title')
+        title.classList.add('quiz__title_hide-after')
       } else {
-        if (slides[currentSlide].querySelector('input:checked').value === "Да") {
-          currentSlide += 1
-        } else {
-          const notes = slides[currentSlide + 2].querySelectorAll('.quiz__note')
-          for (let i = 0; i < notes.length; i++) {
-            notes[i].style.display = "none"
-          }
+        const notes = slides[currentSlide + 2].querySelectorAll('.quiz__note')
+        for (let i = 0; i < notes.length; i++) {
+          notes[i].style.display = "none"
         }
       }
     }
@@ -122,17 +165,44 @@ function quizInit () {
       currentSlide += 1
       slides[currentSlide].classList.add('active')
       initValidate()
+      if (currentSlide === slides.length - 1) initValidateLastSlide()
     } else {
-      form.submit()
+      sendData()
     }
     progressFill.style.width = Math.round(currentSlide/(slides.length - 1) * 100) + '%'
+  }
+
+  function prevSlide () {
+    if (currentSlide > 0) {
+      if (currentSlide === 1) {
+        progress.classList.add('hide')
+        wrap.classList.add('quiz__wrap_first-slide')
+      }
+      if (currentSlide === 4 && slides[2].querySelector('input:checked').value === "Да") {
+        currentSlide -= 1
+      }
+
+      const oldActiveSlide = quiz.querySelector('.quiz__inner.active')
+      oldActiveSlide.classList.remove('active')
+      currentSlide -= 1
+      slides[currentSlide].classList.add('active')
+
+      progressFill.style.width = Math.round(currentSlide/(slides.length - 1) * 100) + '%'
+    }
+  }
+
+  function sendData () {
+    form.submit()
+    setTimeout(() => {
+      location.assign('/quiz-thanks.html')
+    }, 100)
   }
 
   function generateList (list) {
     const type = list.getAttribute('data-type')
     const name = list.getAttribute('data-name')
     const array = (generateData[list.getAttribute('data-list')])[classChosen]
-
+    list.innerHTML = ''
     for (let i = 0; i < array.length; i++) {
       const item = `<label class="quiz__label">
         <input class="quiz__input" type="${type}" name="${name}" value="${array[i]}">
