d5599516fc60304a6bcbe30750650485338d33c5
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-01-22 20:21:24 +0300

    отправка формы

--- assets/scripts/components/quiz.js
+++ assets/scripts/components/quiz.js
@@ -76,24 +76,39 @@ function quizInit () {
   const prevBtns = quiz.querySelectorAll('[data-element="quiz-prev"]')
   const slides = quiz.querySelectorAll('[data-element="quiz-inner"]')
   const form = quiz.querySelector('[data-element="quiz-form"]')
+  const linkTo = form.getAttribute("data-docex")
   const wrap = quiz.querySelector('.quiz__wrap')
   const progress = quiz.querySelector('[data-element="quiz-progress"]')
   const progressFill = quiz.querySelector('[data-element="quiz-progress-bar-fill"]')
 
   function initValidate () {
     const button = slides[currentSlide].querySelector("[data-element='quiz-next']")
-    const input = slides[currentSlide].getElementsByTagName('input')
+    const input = slides[currentSlide].querySelectorAll('.quiz__input')
     if (input[0].type === 'text') {
       for (let i = 0; i < input.length; i++) {
         input[i].addEventListener('input', function () {
           button.disabled = !input[0].value || !input[1].value
         })
       }
-    } else {
+    } else if (input[0].type === 'radio') {
       for (let i = 0; i < input.length; i++) {
         input[i].addEventListener('input', function () {
           const checked = slides[currentSlide].querySelector("input:checked")
           button.disabled = !checked
+        })
+      }
+    } else {
+      const hidden = slides[currentSlide].querySelector('.quiz__hidden-checkbox')
+      for (let i = 0; i < input.length; i++) {
+        input[i].addEventListener('input', function () {
+          const checked = slides[currentSlide].querySelectorAll("input:checked")
+          button.disabled = !checked.length
+          let temp = ''
+          for (let j = 0; j < checked.length; j++) {
+            temp += checked[j].value
+            if (j !== checked.length - 1) temp += ', '
+          }
+          hidden.value = temp
         })
       }
     }
@@ -109,12 +124,12 @@ function quizInit () {
       if (inputs[inputs.length - 1].checked) {
         for (let i = 0; i < inputs.length - 1; i++) {
           inputs[i].disabled = true
-          inputs[i].closest('.quiz__label').classList.add('disable')
+          inputs[i].closest('.quiz__label')?.classList?.add('disable')
         }
       } else {
         for (let i = 0; i < inputs.length - 1; i++) {
           inputs[i].disabled = false
-          inputs[i].closest('.quiz__label').classList.remove('disable')
+          inputs[i].closest('.quiz__label')?.classList?.remove('disable')
         }
       }
       let disableLastInput = false
@@ -143,20 +158,31 @@ function quizInit () {
   initValidate()
 
   function nextSlide () {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
     if (currentSlide === 0) {
       progress.classList.remove('hide')
       wrap.classList.remove('quiz__wrap_first-slide')
     }
     if (currentSlide === 1) {
       classChosen = slides[currentSlide].querySelector('input:checked').value
+      const button = slides[currentSlide + 2].querySelector('.quiz__button')
       if (classChosen === "0") {
         currentSlide += 1
+        button.type = 'submit'
+        button.textContent = 'Отправить ответы'
+        button.disabled = true
+      } else {
+        button.type = 'button'
+        button.textContent = 'Дальше'
+        button.disabled = true
       }
     } else if (currentSlide === 2) {
       if (slides[currentSlide].querySelector('input:checked').value === "Да") {
         currentSlide += 1
       }
-    } else if (currentSlide === 3 && classChosen === "0") sendData()
+    } else if (currentSlide === 3 && classChosen === "0") {
+      sendData()
+    }
 
     if (currentSlide < slides.length - 1) {
       const generateListNode = slides[currentSlide + 1].querySelector('[data-element="quiz-generate"]')
@@ -199,7 +225,7 @@ function quizInit () {
   function sendData () {
     form.submit()
     setTimeout(() => {
-      location.assign('/quiz-thanks.html')
+      location.assign(linkTo)
     }, 100)
   }
 
@@ -209,11 +235,22 @@ function quizInit () {
     const array = (generateData[list.getAttribute('data-list')])[classChosen]
     list.innerHTML = ''
     for (let i = 0; i < array.length; i++) {
-      const item = `<label class="quiz__label">
-        <input class="quiz__input" type="${type}" name="${name}" value="${array[i]}">
+      const nameInput = type === "radio" ? name : false
+      let item
+      if (nameInput) {
+        item = `<label class="quiz__label">
+        <input class="quiz__input" type="${type}" name="${nameInput}" value="${array[i]}">
         <div class="${'quiz__input-view quiz__input-view_' + type}"></div>
         <div class="quiz__input-text">${array[i]}</div>
       </label>`
+      } else {
+        item = `<label class="quiz__label">
+        <input class="quiz__input" type="${type}" value="${array[i]}">
+        <div class="${'quiz__input-view quiz__input-view_' + type}"></div>
+        <div class="quiz__input-text">${array[i]}</div>
+      </label>`
+      }
+
       list.innerHTML += item
     }
   }
