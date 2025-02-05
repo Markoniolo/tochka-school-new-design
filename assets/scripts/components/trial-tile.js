f361dbebfd09631a8ed4bc96ad9e0280ffa38946
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-02-05 19:54:35 +0300

    добавил страницу пробной недели

---
+++ assets/scripts/components/trial-tile.js
@@ -0,0 +1,76 @@
+const trialTile = document.getElementById("trial-tile")
+
+if (trialTile) trialTileInit()
+
+function trialTileInit () {
+  const openers = trialTile.querySelectorAll("[data-element='trial-tile-filter-opener']")
+
+  const reset = trialTile.querySelector("[data-element='trial-tile-filter-reset']")
+  reset.addEventListener('click', resetFilters)
+
+  function resetFilters () {
+    for (let i = 0; i < openers.length; i++) {
+      const wrap = openers[i].nextElementSibling
+      const items = wrap.querySelectorAll("[data-element='trial-tile-filter-input']")
+      items.forEach((item) => {
+        item.checked = false
+      })
+      updateFilter(openers[i], wrap)
+    }
+  }
+
+  for (let i = 0; i < openers.length; i++) {
+    openers[i].addEventListener("click", ()=> openFilter(openers[i]))
+    const wrap = openers[i].nextElementSibling
+    const button = wrap.querySelector("[data-element='trial-tile-filter-save']")
+    button.addEventListener("click", ()=> closeFilter(openers[i], wrap))
+    const items = wrap.querySelectorAll("[data-element='trial-tile-filter-input']")
+    items.forEach((item) => {
+      item.addEventListener('change', () => updateFilter(openers[i], wrap))
+    })
+  }
+
+  function updateFilter (opener, wrap) {
+    opener.innerHTML = opener.getAttribute('data-default-text')
+    const items = wrap.querySelectorAll(["input:checked"])
+    if (items.length) {
+      opener.innerHTML = 'Выбрано: '
+      items.forEach((item) => {
+        const span = document.createElement("span")
+        span.innerHTML = item.nextElementSibling.innerHTML
+        span.classList.add('span')
+        opener.append(span)
+      })
+    }
+    const itemsAll = trialTile.querySelectorAll(["input:checked"])
+    if (itemsAll.length) {
+      reset.style.display = 'flex'
+    } else {
+      reset.style.display = 'none'
+    }
+  }
+
+  function openFilter (opener) {
+    if (opener.classList.contains('active')) {
+      closeFilter(opener)
+    } else {
+      const oldOpen = trialTile.querySelector(".trial-tile__filter-top.active")
+      if (oldOpen) oldOpen.classList.remove('active')
+      opener.classList.add('active')
+    }
+  }
+
+  function closeFilter (opener) {
+    opener.classList.remove('active')
+    for (let i = 0; i < openers.length; i++) {
+      const wrap = openers[i].nextElementSibling
+      const items = wrap.querySelectorAll(["input:checked"])
+      let result = ''
+      items.forEach((item, i) => {
+        if (i > 0) result += '|'
+        result += item.value
+      })
+      console.log(result)
+    }
+  }
+}
