ac42f45d7d5fef2a2e1c54af914ea4fedc0ae832
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-02-25 19:01:59 +0300

    fix

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -458,6 +458,42 @@ function headerInit () {
   }
 }
 
+const howSlider = document.querySelector('[data-element="how-slider"]')
+
+if (howSlider) howSliderInit()
+
+function howSliderInit () {
+  let howSliderSwiper
+
+  function initSlider () {
+    howSliderSwiper = new Swiper(howSlider, {
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      a11y: false,
+      navigation: {
+        nextEl: '.how__nav-btn_next',
+        prevEl: '.how__nav-btn_prev',
+      },
+      scrollbar: {
+        el: '.how__scrollbar',
+        draggable: true,
+      },
+    })
+  }
+
+  window.addEventListener('resize', checkSlider)
+
+  checkSlider()
+
+  function checkSlider () {
+    if (window.innerWidth >= 1440) {
+      initSlider()
+    } else {
+      howSliderSwiper?.destroy()
+    }
+  }
+}
+
 const modalOrderNewSelect = document.querySelector('.modal-order-new__select')
 
 if (modalOrderNewSelect) modalOrderNewSelectInit()
