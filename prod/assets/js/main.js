7a3243d2d70ced642c4e813db5ba9544a8043539
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-04-02 11:40:51 +0300

    update predzaps animation

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1144,23 +1144,30 @@ const predzapsTop = document.querySelector('.predzaps-top')
 if (predzapsTop) predzapsTopInit()
 
 function predzapsTopInit () {
+  const stickyHeader = document.querySelector('.sticky-header')
   const close = predzapsTop.querySelector('.predzaps-top__close')
   close.addEventListener('click', removePredzapsTop)
 
   window.addEventListener('scroll', checkScroll, { passive: true })
 
   function checkScroll () {
-    if (window.scrollY > 0) {
-      predzapsTop.classList.add('hide')
-    } else {
-      predzapsTop.classList.remove('hide')
+    if (window.scrollY > 100) {
+      stickyHeader.classList.add('predzaps-top-hide')
+    } else if (window.scrollY <= 20) {
+      stickyHeader.classList.remove('predzaps-top-hide')
     }
   }
 
   function removePredzapsTop (e) {
     e.preventDefault()
-    predzapsTop.classList.add('hide')
-    setTimeout(() => predzapsTop.remove(), 300)
+    stickyHeader.classList.add('predzaps-top-hide')
+    setTimeout(() => {
+      stickyHeader.classList.add('no-transform')
+      stickyHeader.classList.remove('predzaps-top-hide')
+      window.removeEventListener('scroll', checkScroll, { passive: true })
+      predzapsTop.classList.add('hide')
+      setTimeout(() => predzapsTop.remove(), 600)
+    }, 300)
   }
 }
 
