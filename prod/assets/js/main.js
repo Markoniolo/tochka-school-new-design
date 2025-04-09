a71b458376192fec9c698e365910a4e229589df0
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-04-09 22:27:01 +0300

    teacher course page build

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1276,6 +1276,34 @@ function predzapsTopInit () {
   }
 }
 
+const teacherCost = document.querySelector('.teacher-cost')
+
+if (teacherCost) teacherCostInit()
+
+function teacherCostInit() {
+  const btn = teacherCost.querySelector('.cost__button')
+  const checkbox = teacherCost.querySelector('.cost__teacher-checkbox')
+  const price = teacherCost.querySelector('.cost__price-current')
+
+  checkbox.addEventListener('change', checkboxChangeHandler)
+
+  function checkboxChangeHandler () {
+    const newPrice = checkbox.getAttribute('new-price')
+    const oldPrice = btn.getAttribute('old-price')
+
+    const newHref = checkbox.getAttribute('new-href')
+    const oldHref = btn.getAttribute('old-href')
+
+    if (checkbox.checked) {
+      price.textContent = newPrice
+      btn.href = newHref
+    } else {
+      price.textContent = oldPrice
+      btn.href = oldHref
+    }
+  }
+}
+
 const quiz = document.querySelector('[data-element="quiz"]')
 
 if (quiz) quizInit()
