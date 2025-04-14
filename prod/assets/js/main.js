280f034a0d639d56c3eed747f67aa332f2f02c79
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2025-04-14 11:14:48 +0300

    library page done build

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -919,6 +919,170 @@ function howInit () {
   })
 }
 
+const libraryCap = document.querySelector('.library-cap')
+
+if (libraryCap) libraryCapInit()
+
+function libraryCapInit () {
+  const openers = libraryCap.querySelectorAll("[data-element='library-cap-filter-opener']")
+  const grades = libraryCap.querySelector("[data-ftype='class_select']")
+  const subjects = libraryCap.querySelector("[data-ftype='class_subjects']")
+
+  const save = libraryCap.querySelector(".library-cap__button")
+  if (save) save.addEventListener('click', saveHandler)
+
+  function saveHandler () {
+    let valid = true
+    for (let i = 0; i < openers.length; i++) {
+      const wrap = openers[i].nextElementSibling
+      const itemChecked = wrap.querySelector(["input:checked"])
+      if (!itemChecked) {
+        valid = false
+        openers[i].classList.add('error')
+      }
+    }
+
+    if (valid) {
+      makeFiltration()
+      scrollToTile()
+    }
+  }
+
+  const reset = libraryCap.querySelector("[data-element='library-cap-filter-reset']")
+  if (reset) reset.addEventListener('click', resetFilters)
+
+  function resetFilters () {
+    for (let i = 0; i < openers.length; i++) {
+      const wrap = openers[i].nextElementSibling
+      const items = wrap.querySelectorAll("[data-element='library-cap-filter-input']")
+      items.forEach((item) => {
+        item.checked = false
+      })
+      updateFilter(openers[i], wrap)
+    }
+    makeFiltration()
+  }
+
+  function makeFiltration () {
+    const grades_wrap = grades.nextElementSibling
+    const grades_items = grades_wrap.querySelectorAll(["input:checked"])
+    let grades_result = ''
+    let grade_first = 0
+    grades_items.forEach((item, i) => {
+      if (i > 0) grades_result += '|'
+      if (grade_first == 0) grade_first = item.value
+      grades_result += item.value
+    })
+    let subjects_result = ''
+    try{
+      const subjects_wrap = subjects.nextElementSibling
+      const subjects_items = subjects_wrap.querySelectorAll(["input:checked"])
+      subjects_items.forEach((item, i) => {
+        if (i > 0) subjects_result += '|'
+        subjects_result += item.value
+      })
+    }catch(e){}
+    //   let promo_f = libraryCap.getAttribute('data-promo');
+    //console.log(grades_result)
+    //console.log(subjects_result)
+    let utm_f = libraryCap.getAttribute('data-utm');
+    $('.filtered_elements').html("<div class='tile-loader'></div>");
+    // if(promo_f != null && promo_f != '' && promo_f != undefined){
+    //     $.request('AllCourseFunctions::onPaginateAllCourses', {
+    //             data: {
+    //               'grade': grades_result,
+    //               'subject': subjects_result,
+    //               'promo': promo_f,
+    //             }
+    //         })
+    // }else{
+    $.request('AllCourseFunctions::onPaginateAllSCourses', {
+      data: {
+        'grade': grades_result,
+        'subject': subjects_result,
+        'utm_t': utm_f
+      }
+    })
+    if (grade_first > 0){
+      let url = new URL(window.location.href)
+      url.searchParams.set('gr', grade_first);
+      history.replaceState(null, "", url.toString())
+    }
+    // }
+
+  }
+
+  function scrollToTile () {
+    const banner = document.querySelector('.discount')
+    const bannerHeight = banner ? banner.clientHeight : 0
+    const theElement = document.getElementById('library-tile')
+    let offset
+    if (window.innerWidth < 744) {
+      offset = 84 + bannerHeight
+    } else if (window.innerWidth < 1200) {
+      offset = 102 + bannerHeight
+    } else {
+      offset = 120 + bannerHeight
+    }
+    window.scrollTo(0, theElement.getBoundingClientRect().top + scrollY - offset)
+  }
+
+  for (let i = 0; i < openers.length; i++) {
+    openers[i].addEventListener("click", ()=> openFilter(openers[i]))
+
+    const wrap = openers[i].nextElementSibling
+    const items = wrap.querySelectorAll("[data-element='library-cap-filter-input']")
+    items.forEach((item) => {
+      item.addEventListener('change', () => updateFilter(openers[i], wrap))
+    })
+  }
+
+  function openFilter (opener) {
+    opener.classList.remove('error')
+    if (opener.classList.contains('active')) {
+      closeFilter(opener)
+    } else {
+      const oldOpen = libraryCap.querySelector(".library-cap__filter-top.active")
+      if (oldOpen) oldOpen.classList.remove('active')
+      opener.classList.add('active')
+
+      const filter = opener.parentElement
+      const oldFilter = libraryCap.querySelector(".library-cap__filter.open")
+      if (oldFilter) oldFilter.classList.remove('open')
+      if (filter) filter.classList.add('open')
+    }
+  }
+
+  function closeFilter (opener) {
+    opener.classList.remove('active')
+    const filter = opener.parentElement
+    if (filter) filter.classList.remove('open')
+  }
+
+  function updateFilter (opener, wrap) {
+    opener.innerHTML = opener.getAttribute('data-default-text')
+    const items = wrap.querySelectorAll(["input:checked"])
+    if (items.length) {
+      opener.innerHTML = 'Выбрано: '
+      items.forEach((item) => {
+        const span = document.createElement("span")
+        span.innerHTML = item.nextElementSibling.innerHTML
+        span.classList.add('span')
+        opener.append(span)
+      })
+    }
+    const itemsAll = libraryCap.querySelectorAll(["input:checked"])
+    if (itemsAll.length) {
+      if (reset) reset.classList.add('show')
+    } else {
+      if (reset) reset.classList.remove('show')
+    }
+    closeFilter(opener)
+  }
+
+
+}
+
 const modalOrderNewSelect = document.querySelector('.modal-order-new__select')
 
 if (modalOrderNewSelect) modalOrderNewSelectInit()
@@ -1276,9 +1440,9 @@ function predzapsTopInit () {
   }
 }
 
-const teacherCostArray = document.querySelectorAll('.cost__area')
+const teacherCostArray = document.querySelectorAll('.teacher-cost .cost__area')
 
-if (teacherCostArray) teacherCostArrayInit()
+if (teacherCostArray.length) teacherCostArrayInit()
 
 function teacherCostArrayInit () {
   for (let i = 0; i < teacherCostArray.length; i++) {
@@ -1616,7 +1780,7 @@ function animateScrollToAnchor (theElement, offset) {
     }
   }
   const positionNow = window.pageYOffset
-  const positionElement = theElement.getBoundingClientRect().top + pageYOffset - offset
+  const positionElement = theElement.getBoundingClientRect().top + scrollY - offset
   const duration = 200
   const step = positionElement - positionNow
   const start = performance.now()
@@ -1702,8 +1866,9 @@ if (trialTile) trialTileInit()
 
 function trialTileInit () {
   const config = { attributes: true, childList: true, characterData: true, subtree: true }
-
   const openers = trialTile.querySelectorAll("[data-element='trial-tile-filter-opener']")
+  const grades = trialTile.querySelector("[data-ftype='class_select']")
+  const subjects = trialTile.querySelector("[data-ftype='class_subjects']")
 
   const observer = new MutationObserver(function() {
     const wrap = openers[1].nextElementSibling
