c393c528dc7fb4037a614960687ca5c815c301a0
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-05-07 19:27:21 +0300

    update tile__family and all-courses js

--- assets/scripts/components/button-data-href-links.js
+++
@@ -1,8 +0,0 @@
-const buttonDataHrefLinks = document.querySelectorAll('button[data-href-link]')
-
-for (let i = 0; i < buttonDataHrefLinks.length; i++) {
-  buttonDataHrefLinks[i].addEventListener('click', function (e) {
-    e.preventDefault()
-    window.open(this.getAttribute('data-href-link'), '_blank')
-  })
-}
