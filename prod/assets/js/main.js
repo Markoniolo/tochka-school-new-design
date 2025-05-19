842d12ef6ab6178d6242c5e97454fd2e400b4bdb
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-05-19 18:24:17 +0300

    build update modal-order-new and mobile menu

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1810,19 +1810,37 @@ function libraryCapInit () {
   }
 }
 
-const modalOrderNewSelect = document.querySelector('.modal-order-new__select')
+const modalOrderNewSelect = document.querySelector(".modal-order-new__select")
 
 if (modalOrderNewSelect) modalOrderNewSelectInit()
 
 function modalOrderNewSelectInit () {
-  modalOrderNewSelect.addEventListener('change', function () {
-    const parent = this.closest('.custom-select-container')
-    if (parent) {
-      const span = parent.querySelector('.custom-select-opener span')
-      span.style.color = '#000'
-    }
-  })
+  const top = modalOrderNewSelect.querySelector('.modal-order-new__select-top')
+  const placeholder = modalOrderNewSelect.querySelector('.modal-order-new__select-placeholder')
+  const inputs = modalOrderNewSelect.querySelectorAll('.modal-order-new__select-input')
+
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('change', setValue)
+  }
+
+  top.addEventListener('click', openSelect)
+  window.addEventListener('click', closeSelect)
+
+  function openSelect (e) {
+    e.stopPropagation()
+    top.classList.toggle('active')
+  }
+
+  function closeSelect () {
+    top.classList.remove('active')
+  }
+
+  function setValue () {
+    placeholder.innerHTML = this.parentElement.querySelector('.modal-order-new__select-view').innerHTML
+  }
 }
+
+
 
 const nav = document.querySelector('.header .nav')
 
