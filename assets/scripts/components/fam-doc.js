bb817e595f8c8fd02d24ac6bae3a08b90a7214d1
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-06-06 14:14:44 +0300

    fixes

--- assets/scripts/components/fam-doc.js
+++ assets/scripts/components/fam-doc.js
@@ -4,5 +4,15 @@ if (famDoc) famDocInit()
 
 function famDocInit () {
   const toggle = famDoc.querySelector('.fam-doc__button')
-  toggle.addEventListener('click', () => famDoc.classList.toggle('open'))
+  toggle.addEventListener('click', toggleDoc)
+
+  function toggleDoc () {
+    if (famDoc.classList.contains('open')) {
+      famDoc.classList.remove('open')
+      toggle.innerHTML = 'Открыть расписание аттестации'
+    } else {
+      famDoc.classList.add('open')
+      toggle.innerHTML = 'Скрыть расписание аттестации'
+    }
+  }
 }
