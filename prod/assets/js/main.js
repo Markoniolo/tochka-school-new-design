3628f584922c8621f062b50ee3fdd22824128998
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-07-08 10:04:31 +0300

    build fixes summer timer and add banners

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1213,6 +1213,21 @@ if (preCapForm) globalFormInit(preCapForm, 'onSendPreSubscribeMessage', 'preForm
 const preRegForm = document.querySelector("[data-element='pre-reg__form']")
 if (preRegForm) globalFormInit(preRegForm, 'onSendPreSubscribeMessage', 'preFormData')
 
+const libraryPopupForm = document.querySelector("[data-element='library-popup']")
+if (libraryPopupForm) globalFormInit(libraryPopupForm, 'onSendLibraryM', 'libraryPopupFormData')
+
+function libraryPopupFormData (globalForm) {
+  return {
+    'name': globalForm.querySelector("[name='name']").value,
+    'utm': globalForm.querySelector("[name='utm']").value,
+    'tel': globalForm.querySelector("[name='tel']").value,
+    'email': globalForm.querySelector("[name='email']").value,
+    'policy': globalForm.querySelector("[name='policy']").checked,
+    'news': globalForm.querySelector("[name='news']").checked,
+    'page_name': globalForm.querySelector("[name='page_name']").value,
+  };
+}
+
 function preFormData (globalForm) {
   return {
     'name': globalForm.querySelector("[name='name']").value,
@@ -1317,22 +1332,30 @@ function globalFormInit (form, func_name, type) {
   let iti
 
   if (input) {
-    iti = window.intlTelInput(input, {
-      utilsScript: "../libs/intlTelInputWithUtils.min",
-      initialCountry: 'ru',
-      separateDialCode: true,
-      strictMode: true
-    })
+    if(type == 'libraryPopupFormData'){
+      input.addEventListener('input', function () {
+        let tempValue = input.value
+        const cleanNumber = tempValue.replace(/[^+\d]/g, '')
+        inputHidden.value = cleanNumber
+      })
+    }else{
+      iti = window.intlTelInput(input, {
+        utilsScript: "../libs/intlTelInputWithUtils.min",
+        initialCountry: 'ru',
+        separateDialCode: true,
+        strictMode: true
+      })
 
-    input.addEventListener('input', function () {
-      let tempValue = input.value
-      inputHidden.value = input.value
-      const cleanNumber = tempValue.replace(/[^+\d]/g, '')
-      if (iti.selectedCountryData.dialCode === "7" && cleanNumber.length > 10) {
-        inputHidden.value = cleanNumber.substring(cleanNumber.length - 10)
-      }
-      inputHidden.value = iti.selectedCountryData.dialCode + ' ' + inputHidden.value
-    })
+      input.addEventListener('input', function () {
+        let tempValue = input.value
+        inputHidden.value = input.value
+        const cleanNumber = tempValue.replace(/[^+\d]/g, '')
+        if (iti.selectedCountryData.dialCode === "7" && cleanNumber.length > 10) {
+          inputHidden.value = cleanNumber.substring(cleanNumber.length - 10)
+        }
+        inputHidden.value = iti.selectedCountryData.dialCode + ' ' + inputHidden.value
+      })
+    }
   }
 
   function resetError () {
@@ -1344,7 +1367,7 @@ function globalFormInit (form, func_name, type) {
     e.preventDefault()
     if (input && !input?.value?.trim()) {
       input.classList.add("error")
-    } else if (iti?.isValidNumber() || !input) {
+    } else if ((iti?.isValidNumber() || !input || type == 'libraryPopupFormData')) {
 
       if (type == 'externalFormData') {
         var form_data = externalFormData(globalForm);
@@ -1362,6 +1385,8 @@ function globalFormInit (form, func_name, type) {
         var form_data = preFormData(globalForm);
       } else if (type == 'preFormData') {
         var form_data = preFormData(globalForm);
+      } else if (type == 'libraryPopupFormData') {
+        var form_data = libraryPopupFormData(globalForm);
       }
 
       const email = globalForm.querySelector('[name="email"]')
@@ -1385,7 +1410,13 @@ function globalFormInit (form, func_name, type) {
           isValid = false
         }
       }
-
+      //   if (classSelect) {
+      //     if (!classSelect.value) {
+      //       const opener = classSelect.closest('.custom-select-container').querySelector('.custom-select-opener')
+      //       opener.classList.add('error')
+      //       isValid = false
+      //     }
+      //   }
       if (classSelectTop) {
         const checkedClass = document.querySelector('.modal-order-new__select-input:checked')
         if (!checkedClass) {
@@ -1402,7 +1433,8 @@ function globalFormInit (form, func_name, type) {
         btnSubmit.disabled = true;
         if (type === 'preFormData') {
           e.stopPropagation();
-          btnSubmit.classList.add('loading')
+          const loader = document.querySelector('.form-loader')
+          if (loader) loader.classList.add('active')
           $.request('MainFunctions::onSendPreSubscribeMessage', {
             data: form_data,
           });
@@ -1419,7 +1451,7 @@ function globalFormInit (form, func_name, type) {
           globalForm.submit();
           setTimeout(() => {
             clearForm();
-            location.assign(linkTo + `?cemail=${email_value}`);
+            // location.assign(linkTo + `?cemail=${email_value}`);
             // location.assign(linkTo + '?email='+email_value)
             location.assign(linkTo);
           }, 100)
