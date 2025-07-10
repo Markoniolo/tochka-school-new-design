516ef5f0c3b5dbdfae5295e187e5d28fb75b9c39
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-07-10 22:32:18 +0300

    add review-modal and fixes

--- assets/scripts/components/reviews.js
+++ assets/scripts/components/reviews.js
@@ -61,7 +61,6 @@ function reviewsSliderInit () {
   }
 
   function initInner (inner) {
-    const text = inner.querySelector('.reviews__text')
     const btn = inner.querySelector('.reviews__toggle')
 
     if (inner.clientHeight > 260) {
@@ -76,10 +75,103 @@ function reviewsSliderInit () {
         inner.classList.remove('hide')
         btn.innerHTML = 'Скрыть'
         inner.classList.add('show')
+        createModal(inner, btn)
       } else {
         inner.classList.add('hide')
         btn.innerHTML = 'Читать весь отзыв'
         inner.classList.remove('show')
+      }
+    }
+
+    function createModal(innerOld) {
+      const slide = innerOld.closest('.reviews__slide')
+      const videoOld = slide.querySelector('.reviews__video')
+      const imageOld = slide.querySelector('.reviews__image')
+      const textOld = slide.querySelector('.reviews__text')
+
+      const modal = document.createElement('div')
+      modal.classList.add('review-modal')
+
+      const layer = document.createElement('div')
+      layer.classList.add('review-modal__layer')
+
+      const inner = document.createElement('div')
+      inner.classList.add('review-modal__inner')
+
+      const close = document.createElement('button')
+      close.classList.add('review-modal__close')
+
+      const left = document.createElement('div')
+      left.classList.add('review-modal__left')
+
+      const box = document.createElement('div')
+      box.classList.add('review-modal__box')
+
+      const image = document.createElement('img')
+      image.classList.add('review-modal__image')
+
+      const name = document.createElement('div')
+      name.classList.add('review-modal__name')
+
+      const note = document.createElement('div')
+      note.classList.add('review-modal__note')
+
+      const right = document.createElement('div')
+      right.classList.add('review-modal__right')
+
+      const text = document.createElement('div')
+      text.classList.add('review-modal__text')
+
+      image.src = imageOld.src
+
+      if (videoOld) {
+        const play = document.createElement('div')
+        play.classList.add('review-modal__play')
+        const video = document.createElement('video')
+        video.classList.add('review-modal__video')
+        box.classList.add('review-modal__box_video')
+        video.src = videoOld.src
+        box.append(play)
+        box.append(video)
+
+        box.addEventListener('click', playVideo)
+        video.addEventListener('click', stopVideo)
+
+        function playVideo () {
+          video.play()
+          box.classList.add('active')
+        }
+
+        function stopVideo (e) {
+          e.stopPropagation()
+          video.pause()
+          box.classList.remove('active')
+        }
+      }
+
+      box.append(image)
+      left.append(box)
+      left.append(name)
+      left.append(note)
+      text.innerHTML = textOld.innerHTML
+      right.append(text)
+      inner.append(close)
+      inner.append(left)
+      inner.append(right)
+      modal.append(layer)
+      modal.append(inner)
+
+      layer.addEventListener('click', closeModal)
+      close.addEventListener('click', closeModal)
+
+      const main = document.querySelector('main')
+      main.append(modal)
+
+      function closeModal () {
+        modal.remove()
+        innerOld.classList.add('hide')
+        btn.innerHTML = 'Читать весь отзыв'
+        innerOld.classList.remove('show')
       }
     }
   }
