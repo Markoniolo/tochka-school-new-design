78c0d3b9f7dcdcdbfc31a4598823c72c1ae7a6bb
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-07-28 19:49:49 +0300

    add it page

---
+++ assets/scripts/components/it-how-slider.js
@@ -0,0 +1,40 @@
+const itHowSlider = document.querySelector(".it-how__slider")
+
+if (itHowSlider) itHowSliderInit()
+
+function itHowSliderInit () {
+  let itHowSliderSwiper
+
+  function initSlider () {
+    itHowSliderSwiper = new Swiper(itHowSlider, {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      a11y: false,
+      navigation: {
+        nextEl: '.it-how__nav-btn_next',
+        prevEl: '.it-how__nav-btn_prev',
+      },
+      scrollbar: {
+        el: '.it-how__scrollbar',
+        draggable: true,
+      },
+      breakpoints: {
+        1440: {
+          spaceBetween: 30,
+        }
+      }
+    })
+  }
+
+  window.addEventListener('resize', checkSlider)
+
+  checkSlider()
+
+  function checkSlider () {
+    if (window.innerWidth >= 744) {
+      initSlider()
+    } else {
+      itHowSliderSwiper?.destroy()
+    }
+  }
+}
