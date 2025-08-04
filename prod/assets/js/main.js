5347dd5b46236cce782055c3cdde1f92cd4b7a10
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-08-04 14:12:42 +0300

    build add modal-form-data-attributes

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -2330,6 +2330,51 @@ function maxDiscountInit () {
   }
 }
 
+$("[data-fancybox]").fancybox({
+  beforeShow: function(instance){
+    const form = document.querySelector(this.src)
+    const openButton = instance.current.opts.$orig
+    if (form) {
+      setDataFtitle(openButton, form)
+      setBannerName(openButton, form)
+    }
+  },
+  afterClose: function() {
+    const form = document.querySelector(this.src)
+    if (form) {
+      setDefaultDataFtitle(form)
+      setDefaultBannerName(form)
+    }
+  }
+})
+
+function setBannerName (button, form) {
+  const input = form.querySelector('input[name="banner_v"]')
+  const value = button.attr('data-banner')
+  if (input && value) input.value = value
+}
+
+function setDefaultBannerName (form) {
+  const input = form.querySelector('input[name="banner_v"]')
+  if (input) input.value = ''
+}
+
+function setDataFtitle (button, form) {
+  const title = button.attr('data-ftitle')
+  const titleTag = form.querySelector('.modal-order-new__title')
+  if (title && titleTag) {
+    titleTag.innerHTML = title
+  }
+}
+
+function setDefaultDataFtitle (form) {
+  const titleTag = form.querySelector('.modal-order-new__title')
+  if (titleTag) {
+    const defaultTitle = titleTag.getAttribute('data-default-title')
+    if (defaultTitle) titleTag.innerHTML = defaultTitle
+  }
+}
+
 const modalOrderNewSelect = document.querySelector(".modal-order-new__select")
 
 if (modalOrderNewSelect) modalOrderNewSelectInit()
