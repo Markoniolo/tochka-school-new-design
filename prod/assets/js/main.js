75d38f889beabd0b844e0d2175c16265cd271f26
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-08-11 20:00:16 +0300

    build it page fixes

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1800,6 +1800,39 @@ function instructionVideoBoxInit() {
   }, { once: true })
 }
 
+const itCapCards = document.querySelector('.it-cap__cards')
+
+if (itCapCards) itCapCardsInit()
+
+function itCapCardsInit () {
+  let itCapCardsSwiper
+
+  function initSlider () {
+    itCapCardsSwiper = new Swiper(itCapCards, {
+      slidesPerView: 'auto',
+      allowTouchMove: false,
+      spaceBetween: 25,
+      autoplay: {
+        delay: 3000,
+      },
+      speed: 1000,
+      loop: true,
+    })
+  }
+
+  window.addEventListener('resize', checkSlider)
+
+  checkSlider()
+
+  function checkSlider () {
+    if (window.innerWidth >= 1440) {
+      initSlider()
+    } else {
+      itCapCardsSwiper?.destroy()
+    }
+  }
+}
+
 const itHowSlider = document.querySelector(".it-how__slider")
 
 if (itHowSlider) itHowSliderInit()
