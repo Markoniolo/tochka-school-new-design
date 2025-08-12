2eec37e61580133bc6b4277af1b7e4ca84fc1754
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-08-12 17:02:10 +0300

    build update reasons page

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -3247,31 +3247,98 @@ const reasons = document.querySelector('.reasons')
 if (reasons) reasonsInit()
 
 function reasonsInit () {
+  window.scrollTo(0, 0)
+  const body = document.querySelector('body')
+  body.classList.add('no-scroll')
   const area = reasons.querySelector('.reasons__area')
   const circles = reasons.querySelectorAll('.reasons__circle')
+  const caption = reasons.querySelector('.reasons__caption')
+  const image = reasons.querySelector('.reasons__image')
 
   const step = 300
-  let index = 0
-  let interval
+  let index = -1
 
-  setTimeout(
-    ()=> interval = setInterval(animate, 3500),
-    4500
-  )
+  function throttle(func, delay) {
+    let inThrottle
+    let lastFn
+    let lastTime
 
-  setTimeout(() => reasons.style.backgroundPositionY = `${step * (index+1)}px`, 8100)
+    return function() {
+      const context = this
+      const args = arguments
 
-  function animate () {
-    index += 1
-    if (index < 26) {
-      circles[index].classList.add('animate')
-      setTimeout(() => reasons.style.backgroundPositionY = `${step * (index+1)}px`, 3750)
-    } else if (index === 26) {
-      circles[index].classList.add('animate-end')
-      if (area) area.classList.add('end')
-    } else {
-      clearInterval(interval)
+      if (!inThrottle) {
+        func.apply(context, args)
+        lastTime = Date.now()
+        inThrottle = true
+      } else {
+        clearTimeout(lastFn)
+        lastFn = setTimeout(function() {
+          if (Date.now() - lastTime >= delay) {
+            func.apply(context, args)
+            lastTime = Date.now()
+          }
+        }, Math.max(delay - (Date.now() - lastTime), 0))
+      }
     }
+  }
+
+  function update (e) {
+    if (e.deltaY < 0) {
+      prevSlide()
+    } else if (e.deltaY > 0) {
+      nextSlide()
+    }
+  }
+
+  window.addEventListener('wheel', throttle(update, 200), { passive: true })
+
+  function prevSlide () {
+    if (index > -1 && window.scrollY === 0) {
+      body.classList.add('no-scroll')
+      if (area) area.classList.remove('end')
+      hideOldSlide()
+      index--
+      showNewSlide()
+      animateBg()
+      if (index === -1) {
+        caption.classList.remove('animate')
+        image.classList.remove('animate')
+        area.classList.remove('animate')
+        circles[0].classList.remove('animate-end')
+      }
+    }
+  }
+
+  function nextSlide () {
+    if (index < circles.length - 1) {
+      hideOldSlide()
+      index++
+      showNewSlide()
+      animateBg()
+      if (index === 0) {
+        caption.classList.add('animate')
+        image.classList.add('animate')
+        area.classList.add('animate')
+        circles[0].classList.add('animate-end')
+      }
+      if (index === circles.length - 1) {
+        area.classList.add('end')
+        body.classList.remove('no-scroll')
+      }
+    }
+  }
+
+  function hideOldSlide () {
+    if (circles[index]) circles[index].classList.remove('animate')
+  }
+
+  function showNewSlide () {
+    if (circles[index]) circles[index].classList.add('animate')
+  }
+
+  function animateBg () {
+    reasons.style.backgroundPositionY = `${step * (index+1)}px`
   }
 }
 
