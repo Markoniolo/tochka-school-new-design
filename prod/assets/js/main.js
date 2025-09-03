1c576dbf4e4cb62dec5a9e10a97c030f3727308c
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-09-03 20:00:25 +0300

    build fiexs

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -872,6 +872,17 @@ function discountInit () {
   }
 }
 
+const egeCapTabsArray = document.querySelectorAll('.ege-cap__tabs')
+
+for (let i = 0; i < egeCapTabsArray.length; i++) {
+  egeCapTabsInit(egeCapTabsArray[i])
+}
+
+function egeCapTabsInit (tabs) {
+  const tab = tabs.querySelector('a.ege-cap__tab')
+  if (tab) tab.addEventListener('click', () => tabs.classList.add('active'))
+}
+
 const egeTileFilter = document.querySelector(".ege-tile__filter")
 
 if (egeTileFilter) egeTileFilterInit()
@@ -883,12 +894,34 @@ function egeTileFilterInit () {
   const selectedText = egeTileFilter.querySelector('.ege-tile__filter-top-text')
   const inputs = egeTileFilter.querySelectorAll('.ege-tile__filter-input')
   const cards = document.querySelectorAll('.all-courses__item')
+  const tabs = document.querySelectorAll('.ege-tile__tab-input')
+  let classId = false
 
   egeTileFilterTop.addEventListener("click", (e)=> openFilter(egeTileFilterTop, e))
 
   document.addEventListener('click', function () {
     egeTileFilterTop.classList.remove('active')
   })
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('input', tabHandler)
+  }
+
+  function tabHandler() {
+    this.checked = !this.checked
+    if (this.checked) {
+      this.checked = false
+      classId = false
+    } else {
+      const oldActiveTabs = document.querySelectorAll('.ege-tile__tab-input:checked')
+      for (let i = 0; i < oldActiveTabs.length; i++) {
+        oldActiveTabs[i].checked = false
+      }
+      this.checked = true
+      classId = this.value
+    }
+    updateFilter()
+  }
 
   function openFilter (opener, e) {
     e.stopPropagation()
@@ -918,15 +951,34 @@ function egeTileFilterInit () {
   }
 
   function updateFilter () {
-    if (this.value === 'all-subjects') {
-      showAllCards()
+    const subject = egeTileFilter.querySelector('.ege-tile__filter-input:checked')
+    if (subject.value === 'all-subjects') {
+      if (classId) {
+        const activeCards = document.querySelectorAll('[data-class-id="' + classId + '"]')
+        hideAllCards()
+        showActiveCards(activeCards)
+      } else {
+        showAllCards()
+      }
     } else {
       hideAllCards()
-      selectedText.innerHTML = this.getAttribute('data-text')
-      const activeCards = document.querySelectorAll('[data-subject-id="' + this.value + '"]')
-      for (let i = 0; i < activeCards.length; i++) {
-        activeCards[i].style.display = 'flex'
+      selectedText.innerHTML = subject.getAttribute('data-text')
+      const activeCards = document.querySelectorAll('[data-subject-id="' + subject.value + '"]')
+      if (classId && activeCards.length) {
+        const filteredCards = []
+        for (let i = 0; i < activeCards.length; i++) {
+          if (activeCards[i].getAttribute('data-class-id') === classId) filteredCards.push(activeCards[i])
+        }
+        showActiveCards(filteredCards)
+      } else {
+        showActiveCards(activeCards)
       }
+    }
+  }
+
+  function showActiveCards (activeCards) {
+    for (let i = 0; i < activeCards.length; i++) {
+      activeCards[i].style.display = 'flex'
     }
   }
 
