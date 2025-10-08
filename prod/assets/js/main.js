951c2b6cff148b0d8f1c0513e8be28d863f75f75
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-10-08 10:22:01 +0300

    build pre-k page

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -3608,6 +3608,52 @@ for (let i = 0; i < preFaqItems.length; i++) {
   }
 }
 
+const preKTile = document.querySelector('.pre-k-tile')
+
+if (preKTile) preKTileInit()
+
+function preKTileInit () {
+  togglePrice()
+
+  function togglePrice () {
+    const items = preKTile.querySelectorAll(".all-courses__item")
+    items.forEach((item) => {
+      const toggle = item.querySelectorAll('.all-courses__radio-input')
+      if (!toggle.length) return
+      const boxes = item.querySelectorAll('.all-courses__box')
+      const textReplace = item.querySelector('.all-courses__info.text-replace')
+      let texts
+      if (textReplace) {
+        texts = textReplace.querySelectorAll('.all-courses__info-span')
+      }
+      for (let i = 0; i < toggle.length; i++) {
+        toggle[i].addEventListener('change', toggleBoxes)
+        if (texts) toggle[i].addEventListener('change', toggleTexts)
+      }
+
+      function toggleBoxes () {
+        for (let i = 0; i < boxes.length; i++) {
+          if (boxes[i].classList.contains('hide')) {
+            boxes[i].classList.remove('hide')
+          } else {
+            boxes[i].classList.add('hide')
+          }
+        }
+      }
+
+      function toggleTexts () {
+        for (let i = 0; i < texts.length; i++) {
+          if (texts[i].classList.contains('hide')) {
+            texts[i].classList.remove('hide')
+          } else {
+            texts[i].classList.add('hide')
+          }
+        }
+      }
+    })
+  }
+}
+
 const preReviewsSlider = document.querySelector('[data-element="pre-reviews-slider"]')
 
 if (preReviewsSlider) preReviewsSliderInit()
