db73a990ead465bcadabd9ddec818961d299b7ad
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-11-25 12:19:20 +0300

    build educator-detail page

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -1011,6 +1011,26 @@ function discountInit () {
   }
 }
 
+const educatorSkillsItems = document.querySelectorAll('.educator-skills__item')
+
+if (educatorSkillsItems.length) educatorSkillsItemsInit()
+
+function educatorSkillsItemsInit () {
+  const line = document.querySelector('.educator-skills__line')
+
+  function resizeHandler () {
+    if (window.innerWidth < 1440) {
+      line.style.bottom = `${educatorSkillsItems[educatorSkillsItems.length - 1].clientHeight - 4}px`
+    } else {
+      line.style.bottom = '20px'
+    }
+  }
+
+  resizeHandler()
+
+  window.addEventListener('resize', resizeHandler)
+}
+
 const egeCapTabsArray = document.querySelectorAll('.ege-cap__tabs')
 
 for (let i = 0; i < egeCapTabsArray.length; i++) {
