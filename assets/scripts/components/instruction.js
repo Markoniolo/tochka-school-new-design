3c63e88ed4a8fe79c03d45a5922d95fa894fb0f4
markoniolo <140736007+Markoniolo@users.noreply.github.com>
2025-12-18 13:31:55 +0300

    update instruction block

--- assets/scripts/components/instruction.js
+++ assets/scripts/components/instruction.js
@@ -3,9 +3,12 @@ const instructionVideoBox = document.querySelector('.instruction__video-box')
 if (instructionVideoBox) instructionVideoBoxInit()
 
 function instructionVideoBoxInit() {
-  const video = instructionVideoBox.querySelector('video')
-  instructionVideoBox.addEventListener('click', function () {
-    this.classList.add('active')
-    if (video) video.play()
-  }, { once: true })
+  const iframe = instructionVideoBox.querySelector('iframe')
+  if (!iframe) {
+    const video = instructionVideoBox.querySelector('video')
+    instructionVideoBox.addEventListener('click', function () {
+      this.classList.add('active')
+      if (video) video.play()
+    }, { once: true })
+  }
 }
